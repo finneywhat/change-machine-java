9194724c0c2bc139b64869e9ef254289b75fae39
Chris Finney and Evan Bishop <evan@>
2017-03-20 13:51:52 -0700

    add comments to get /coin-combo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,13 +17,20 @@ public class App {
     }, new VelocityTemplateEngine());
 
     get("/coin-combo", (request, response) -> {
+      // Create new HashMap object named model
       Map<String, Object> model = new HashMap<String, Object>();
+      // Parse the input from the form and create new Float named moneyInput
       Float moneyInput = Float.parseFloat(request.queryParams("valueInput"));
+      //create new ChangeMachine so we can call its method
       ChangeMachine master = new ChangeMachine();
+      // call its method
       String moneyOutput = master.makeChange(moneyInput);
+      // use the HashMap to attach moneyOutput to the key/name of form valueInput
       model.put("valueInput", moneyOutput);
       model.put("template", "templates/coin-combo.vtl");
+      // Is this where the key value gets placed into the $ ?
       return new ModelAndView(model, layout);
+      // WTF? new VelocityTemplateEngine?
     }, new VelocityTemplateEngine());
   }
 }
